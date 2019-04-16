b1d968dff35960eac242d65ffc34398e617742f5
MIles <wanderlust.conscious@gmail.com>
2019-04-16 16:30:45 -0700

    'newgit'

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,8 +1,16 @@
-// Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
+// Update the following function to take an argument and output 'Even' if the argument is an even number or
+// 'Odd' if the number is an odd number.
 
-function oddOrEven(){
-    console.log('result')
+function oddOrEven(number){
+  if(number % 2 === 1) {
+    return 'odd'
+  } else if (number % 2 === 0) {
+    return 'even'
+  }
 }
+
+  console.log(oddOrEven(11))
+
 
 oddOrEven(4)
 oddOrEven(11)
